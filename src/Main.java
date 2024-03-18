25442ccf4e234d563b6f0d8919f05f4f0bc08fa9
Vasechko-M <fornewstudies@gmail.com>
2024-03-18 16:54:49 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        //task 1
+        System.out.println("Задача 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+        System.out.println();
+        //task 2
+        System.out.println("Задача 2");
+        String fullNameUp = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullNameUp);
+        System.out.println();
+        //task 3
+        System.out.println("Задача 3");
+        String fullNameS = "Иванов Семён Семёнович";
+        fullNameS = fullNameS.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullNameS);
+        System.out.println();
+        String expectedFullNameS = "Иванов Семен Семенович";
+        if (fullNameS.equals(expectedFullNameS)) {
+            System.out.println("Успех");
+        } else {
+            System.out.println("Неудача");
+        }
+
+
+    }
+}
